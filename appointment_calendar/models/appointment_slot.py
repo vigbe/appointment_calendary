2c64fc110511e6c25f04fa4cc2a952dc2e8f6e44
vigbe <victor.bastiasescobar@gmail.com>
2026-08-30 15:46:12 -0400

    style: apply main reformat and website URL (align with 19.0)

--- appointment_calendar/models/appointment_slot.py
+++ appointment_calendar/models/appointment_slot.py
@@ -1,20 +1,28 @@
-
 from odoo import fields, models
 
 
 class AppointmentSlot(models.Model):
-    _name = 'appointment_calendar.slot'
-    _description = 'Horario de Cita'
+    _name = "appointment_calendar.slot"
+    _description = "Horario de Cita"
 
-    appointment_type_id = fields.Many2one('appointment_calendar.type', string='Tipo de Cita', ondelete='cascade', required=True)
-    weekday = fields.Selection([
-        ('1', 'Lunes'),
-        ('2', 'Martes'),
-        ('3', 'Miércoles'),
-        ('4', 'Jueves'),
-        ('5', 'Viernes'),
-        ('6', 'Sábado'),
-        ('7', 'Domingo'),
-    ], string='Día de la Semana', required=True)
-    start_hour = fields.Float(string='Hora Inicio', required=True)
-    end_hour = fields.Float(string='Hora Fin', required=True)
+    appointment_type_id = fields.Many2one(
+        "appointment_calendar.type",
+        string="Tipo de Cita",
+        ondelete="cascade",
+        required=True,
+    )
+    weekday = fields.Selection(
+        [
+            ("1", "Lunes"),
+            ("2", "Martes"),
+            ("3", "Miércoles"),
+            ("4", "Jueves"),
+            ("5", "Viernes"),
+            ("6", "Sábado"),
+            ("7", "Domingo"),
+        ],
+        string="Día de la Semana",
+        required=True,
+    )
+    start_hour = fields.Float(string="Hora Inicio", required=True)
+    end_hour = fields.Float(string="Hora Fin", required=True)
